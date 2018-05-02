c19b6ab9a143bd5dfe063c50402726587f89ab4c
Wallace Wu <wallace@skrapit.co>
2018-05-02 12:44:44 +0800

    fix build error

--- Sources/Controllers/MessagesViewController.swift
+++ Sources/Controllers/MessagesViewController.swift
@@ -146,7 +146,7 @@ open class MessagesViewController: UIViewController {
         
         let top = messagesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: topLayoutGuide.length)
         let separator = messagesCollectionView.bottomAnchor.constraint(equalTo: messageInputBar.topAnchor)
-        let bottom = messageInputBar.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottomLayoutGuide.length))
+        let bottom = messageInputBar.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: bottomLayoutGuide.length)
         if #available(iOS 11.0, *) {
             let leading = messagesCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
             let trailing = messagesCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
